1207d3c8a3bf012223e739fe28c917c3c9ec3db4
Brad Fabian <bradleofabian@gmail.com>
2021-01-06 12:25:15 -0500

    aded a settimeout to decrimant state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React, {useState} from "react";
-import Button from './components/Button';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 
 function App() {
 
   const [textInput, setTextInput ] = useState('');
+  const [timerValue, setTimerValue] = useState(25);
 
   function handleChange(e) {
     const {value} = e.target
     setTextInput(value)
   }
 
-  console.log(textInput)
+ function calculateWordCount(text) {
+  const wordArr = text.trim().split(" ")
+  return wordArr.filter(word => word !== "").length
+ }
+
+ useEffect(() => {
+   if(timerValue > 0){
+    setTimeout(() => {setTimerValue(time => time -1)}, 1000);
+   }
+  
+   
+ }, [timerValue])
 
   return (
+
+   
     <div className="App">
       <h1>How fast can you type?</h1>
       <textarea value={textInput} onChange={handleChange}>
 
       </textarea>
-      <h4>Time remaining: </h4>
-      <Button/>
+      <h4>Time remaining: {timerValue}</h4>
+      <button onClick={() => calculateWordCount(textInput)}>Start</button>
       <h1> Word Count: </h1>
     </div>
   );
