9c9289f632a2da3a62a535094d11070150d55b23
Brad Fabian <bradleofabian@gmail.com>
2021-01-16 15:11:32 -0500

    finished typing game

--- src/App.js
+++ src/App.js
@@ -1,44 +1,68 @@
 import React, {useState, useEffect} from "react";
+import ReactTypingEffect from 'react-typing-effect';
 import './App.css';
 
 
 function App() {
 
-  const [textInput, setTextInput ] = useState('');
-  const [timerValue, setTimerValue] = useState(25);
-  const [startCount, setStartCount] = useState(false);
-
-  function handleChange(e) {
-    const {value} = e.target
-    setTextInput(value)
-  }
-
- function calculateWordCount(text) {
-  const wordArr = text.trim().split(" ")
-  return wordArr.filter(word => word !== "").length
- }
-
- useEffect(() => {
-   if(startCount && timerValue > 0){
-    setTimeout(() => {setTimerValue(time => time - 1)}, 1000);
-   } else if(timerValue === 0) {
-     setStartCount(false)
-   }
+  const STARTING_TIME = 30
+    
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
   
-   
- }, [timerValue, startCount])
+  function handleChange(e) {
+      const {value} = e.target
+      setText(value)
+  }
+  
+  function calculateWordCount(text) {
+      const wordsArr = text.trim().split(" ")
+      return wordsArr.filter(word => word !== "").length
+  }
+  
+  function startGame() {
+      setIsTimeRunning(true)
+      setTimeRemaining(STARTING_TIME)
+      setText("")
+      setWordCount(0)
+  }
+  
+  function endGame() {
+      setIsTimeRunning(false)
+      setWordCount(calculateWordCount(text))
+      
+  }
+  
+  
+  
+  useEffect(() => {
+      if(isTimeRunning && timeRemaining > 0) {
+          setTimeout(() => {
+              setTimeRemaining(time => time - 1)
+          }, 1000)
+      } else if(timeRemaining === 0) {
+          endGame()
+      }
+  }, [timeRemaining, isTimeRunning])
 
   return (
 
    
     <div className="App">
+      
       <h1>How fast can you type?</h1>
-      <textarea value={textInput} onChange={handleChange}>
+      <textarea value={text} disabled={!isTimeRunning} onChange={handleChange}>
 
       </textarea>
-      <h4>Time remaining: {timerValue}</h4>
-      <button onClick={() => setStartCount(true)}>Start</button>
-      <h1> Word Count: </h1>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning}  >Start</button>
+      <h1> Word Count: {wordCount}</h1>
+
+      <ReactTypingEffect text={["Hand Coded By Me"]}
+       cursorRenderer={cursor => <h1>{cursor}</h1>}
+       />
     </div>
   );
 }
