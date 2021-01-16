89145000539c9b6b52ea2c03f8b8c7f6fd27aaa5
Brad Fabian <bradleofabian@gmail.com>
2021-01-16 16:57:05 -0500

    changed everyhting i business logic to custom hook

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import React, {useState, useEffect, useRef} from "react";
+import React from "react";
 import ReactTypingEffect from 'react-typing-effect';
 import './App.css';
-
+import useCountHook from './components/useCountHook';
 
 function App() {
 
-  const STARTING_TIME = 30
-    
-  const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-  const textRef = useRef(null);
-  
-  function handleChange(e) {
-      const {value} = e.target
-      setText(value)
-  }
-  
-  function calculateWordCount(text) {
-      const wordsArr = text.trim().split(" ")
-      return wordsArr.filter(word => word !== "").length
-  }
-  
-  function startGame() {
-      setIsTimeRunning(true)
-      setTimeRemaining(STARTING_TIME)
-      setText("")
-      setWordCount(0)
-      textRef.current.disabled = false
-      textRef.current.focus();
-  }
-  
-  function endGame() {
-      setIsTimeRunning(false)
-      setWordCount(calculateWordCount(text))
-      
-  }
-  
-  
-  
-  useEffect(() => {
-      if(isTimeRunning && timeRemaining > 0) {
-          setTimeout(() => {
-              setTimeRemaining(time => time - 1)
-          }, 1000)
-      } else if(timeRemaining === 0) {
-          endGame()
-      }
-  }, [timeRemaining, isTimeRunning])
+  const {textRef, text, isTimeRunning ,handleChange, timeRemaining, startGame , wordCount,} = useCountHook();
 
   return (
 
